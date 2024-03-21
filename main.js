7704db1b1e9f74ce0427a404af00701eaae778b2
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:58:35 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+function make_shirt(size, text) {
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+make_shirt('large', 'Hello would');
+make_shirt('medium', 'Hello wold');
+make_shirt('small', 'Hello would');
---
+++ main.ts
@@ -0,0 +1,12 @@
+
+
+function make_shirt(size:string, text:string){
+
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+
+make_shirt('large','Hello would');
+
+make_shirt('medium','Hello wold');
+
+make_shirt('small','Hello would');
